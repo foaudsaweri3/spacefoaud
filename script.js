2dd58ca1d503e84cb51c886255ff496c11088fdb
foaudsaweri3 <foaudsaweri3@gmail.com>
2024-11-26 00:51:51 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Добавляем обработчик события клика на кнопку
-document.addEventListener("DOMContentLoaded", function() {
-    const button = document.createElement("button");
-    button.textContent = "Узнать больше";
-    button.onclick = function() {
-        alert("Ближневосточный барсук - это очень интересное животное!");
-    };
-    document.body.appendChild(button);
-});
